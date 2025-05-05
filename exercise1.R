37c5da3e82c437f9582b570017d0175e593e2fbf
Dr Pawel Orzechowski <24914654+drpawelo@users.noreply.github.com>
2025-05-05 19:01:55 +0100

    final test of deploy action

--- exercise1.R
+++ exercise1.R
@@ -1,1 +1,1 @@
-print('banana 1 test 5: changed the code file in R studio, but did not locally manually run make_html.R in r studio')
+print('banana - test, I am changing exercise1.R locally but not running the make_html.R locally. And then I push')
