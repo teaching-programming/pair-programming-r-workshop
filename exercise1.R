2ed99c4fb9a49634d0390ca10cb4dfd17c61b3e3
Dr Pawel Orzechowski <24914654+drpawelo@users.noreply.github.com>
2025-05-05 18:06:26 +0100

    test 5

--- exercise1.R
+++ exercise1.R
@@ -1,1 +1,1 @@
-print('banana 1 test 2: changed the code file in R studio, but did not locally manually run make_html.R in r studio')
+print('banana 1 test 5: changed the code file in R studio, but did not locally manually run make_html.R in r studio')
