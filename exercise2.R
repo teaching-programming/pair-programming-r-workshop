032bb95c0948c78a6730e23fb6bf9bb56fd9573b
Brittany Blankinship <53411038+bblankinship@users.noreply.github.com>
2025-05-09 19:02:27 +0100

    create part 2 exercises and update instructions HTML

--- exercise2.R
+++ exercise2.R
@@ -1,1 +1,36 @@
-print('banana 2!')
+#### Exercise 2.1: Create a function that called `is_equal` that determines if 2 numeric inputs are the same. Test if the function works as expected with the test below. ####
+
+is_equal <- function (____, ____){
+  if(_____ == ____)
+    return("Yes, these numbers are the same")
+  else 
+    return("No, these numbers are NOT the same")
+}
+
+is_equal(5, 10) # expect No
+is_equal(4, 4) # expect Yes 
+is_equal(57.9, 57.3) # expect No 
+
+#### Exercise 2.2: Create a few more test cases to determine if your `is_equal()` function works as expected.
+
+# Your code here! 
+
+#### Exercise 2.3: Create a function called `is_divisible` that checks if 2 numbers are divisible by each other and takes 2 numeric values as the arguments. ####
+
+# uncomment the code below to get started 
+#is_divisible <- function(____, ____) {
+#  if (____ %% ____ == 0 || ____ %% ____ == 0) {
+#    return(TRUE)
+#  } else {
+#    return(____)
+#  }
+#}
+
+#is_divisible(10, 5) # expect TRUE 
+#is_divisible(57, 4) # expect FALSE
+#is_divisible(562894, 14813) # expect TRUE
+
+
+#### Bonus exercise: Now that you are comfortable with conditional statements and multiple arguments, return to exercise 1 and create a function called `unit_conversion` which combines the 4 functions we created into a single function. ####
+
+# Your code here! Should you wish to accept this challenge, otherwise move to exercise 3 
