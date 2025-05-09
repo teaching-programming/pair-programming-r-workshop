2999fbdf5ada562fb3bdfd83074cdc9d0680e85a
Brittany Blankinship <53411038+bblankinship@users.noreply.github.com>
2025-05-09 18:58:22 +0100

    update exercise delineation with more # for visual clarity

--- exercise1.R
+++ exercise1.R
@@ -1,22 +1,22 @@
-## Exercise 1.1: Create a function called `convert_temperature_CtoF` that takes a numeric value (a temperature in Celsius) as the argument. ----
+#### Exercise 1.1: Create a function called `convert_temperature_CtoF` that takes a numeric value (a temperature in Celsius) as the argument. ####
 
 convert_temp_CtoF <- _______(____) {
   _______ * 9/5 + 32
 }
 
-## Exercise 1.2 Now create a function for conversion the other way called `convert_temperature_FtoC`that takes a numeric value (a temperature in Fahrenheit) as the argument. ----
+#### Exercise 1.2 Now create a function for conversion the other way called `convert_temperature_FtoC`that takes a numeric value (a temperature in Fahrenheit) as the argument. ####
 
 # uncomment the code below to get started 
 #convert_temp_FtoC <- _______(____) {
 #  (_______ - 32) * 5/9)
 #}
 
-## Exercise 1.3 Using your solutions above as a guide, create a function called `convert_length_INtoCM` that takes a numeric value (a unit of length in inches) as the argument.----
+#### Exercise 1.3 Using your solutions above as a guide, create a function called `convert_length_INtoCM` that takes a numeric value (a unit of length in inches) as the argument. ####
 
 # HINT: the inches to cm formula: inches * 2.54 
 
 # Your code here! 
 
-## Exercise 1.4 Now create a function called `convert_length_CMtoIN` that takes a numeric value (a unit of length in centimeters) as the argument.----
+#### Exercise 1.4 Now create a function called `convert_length_CMtoIN` that takes a numeric value (a unit of length in centimeters) as the argument.####
 
 # Your code here! 
