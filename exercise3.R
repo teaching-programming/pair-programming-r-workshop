3f2f0c0312a6b985b21a687c4d6df5fb62d6e127
Brittany Blankinship <53411038+bblankinship@users.noreply.github.com>
2025-05-09 19:45:29 +0100

    Create part 3 exercises and update HTML instructions

--- exercise3.R
+++ exercise3.R
@@ -1,1 +1,34 @@
-print('banana 3kasia')
+#### Exercise 3.1: Create the `categorize_value` function as described above.
+categorize_value <- function(x){
+  if(x _____) {
+    return("low")
+  } else if (x _____) {
+    return("medium")
+  } else {
+    return("high")
+  }
+}
+
+#### Exercise 3.2: Test your `categorize_value()` function on the following cases: 35, 76, 187, 100 - does it work as expected? ####
+
+# your code here! 
+
+#### Exercise 3.3: Now test your `categorize_value()` value function with the `values` vector below. 
+
+values <- c(45, 60, 150, 85, 20)
+
+# your code here! 
+
+#### Exercise 3.4: Oh no! An error - can you think why this error might have occurred? Discuss with your partner(s) ####
+
+# discussion taking place...
+
+#### Exercise 3.5: Not to to fear! Enter the map family of functions from the {purrr} package, which allows us to apply a function to each element of a vector, that is, to take a function that is not vectorized and still use vectorization! Huzzah! ifelse() statements are *NOT* vectorized, hence the above error. Most functions in R are vectorized, meaning that the function will operate on all elements of a vector without needing to loop through and act on each element one at a time. This makes writing code more concise, easy to read, and less error prone. Vectorization is a real superpower of R! ####
+
+library(purrr)
+
+# uncomment the code below to get started. The 2nd argument of `map_chr()` is the function to be applied 
+# map_chr(values, ____)
+
+# Notice, values is a vector with 5 elements, and the output of the map_chr function is a character vector with 5 elements! That is, the result of `categorize_value()` being applied to each element in the `values` vector. 
+
--- make_html.R
+++ make_html.R
@@ -37,9 +37,9 @@ assemble_whole_page <- function(){
   #TODO: use loop to do this once? but that could be c()s in c()s, grrr
   exercise_1_html <- exercise_html("Exercise 1: Unit conversion", "Your task here is to create functions that convert between metric and imperial systems.  The goal of this series of exercises is to understand functions better. Fill in the ____ with appropriate code.", 
                                    "exercise1.R" )
-  exercise_2_html <- exercise_html("Exercise 2: Division", "In Exercise 1 we create 4 functions, 2 to convert temperature units and 2 to convert units of length. An even more ideal solution would be to have a single function that converted a numeric value depending on the conversion of interest (i.e., C_to_F, F_to_C, in_to_cm, or cm_to_in) - enter multiple arguments and conditional statements! We will also be using some test cases to determine if our functions work in the way we would expect.", 
+  exercise_2_html <- exercise_html("Exercise 2: Equality & Division", "In Exercise 1 we create 4 functions, 2 to convert temperature units and 2 to convert units of length. An even more ideal solution would be to have a single function that converted a numeric value depending on the conversion of interest (i.e., C_to_F, F_to_C, in_to_cm, or cm_to_in) - enter multiple arguments and conditional statements! We will also be using some test cases to determine if our functions work in the way we would expect.", 
                                    "exercise2.R" )
-  exercise_3_html <- exercise_html("Exercise 3", "Your task here is to....", "exercise3.R" )
+  exercise_3_html <- exercise_html("Exercise 3: Vectorization", "Your task here is to create a function called `categorize_value` that categorizes values based on the following conditions: less than 50 = low, less than or equal to 100 = medium, otherwise high.", "exercise3.R" )
   exercise_4_html <- exercise_html("Exercise 4", "Your task here is to....", "exercise4.R" )
   
   all_exercises_html <- paste0(exercise_1_html,exercise_2_html,exercise_3_html,exercise_4_html)
